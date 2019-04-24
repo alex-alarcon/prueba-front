6bffb5b9aa39db9c0c7c08eae5c750cf520e5c55
Alexander Alarcon <walarcon14@gmail.com>
2019-04-23 20:15:33 -0500

    Se trae la data para el menu

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
+import { fetchMenu } from '../../services/api';
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  state = {
+    isLoading: true,
+    menu: []
+  };
+
+  componentDidMount() {
+    this.loadMenu();
+  }
+
+  loadMenu() {
+    fetchMenu().then(res => {
+      this.setState({
+        menu: res,
+        isLoading: false
+      });
+    });
+  }
+
+  render() {
+    const loader = <p>Loading...</p>;
+    const { isLoading } = this.state;
+    if (isLoading) {
+      return loader;
+    }
+    return <div className="App" />;
+  }
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const ENDPOINT = {
+  MENU: 'mi-servicio',
+  FORM: 'form'
+};
+
+const instance = axios.create({
+  baseURL: `http://localhost:3001/`
+});
+
+export const fetchMenu = () => {
+  return instance
+    .get(ENDPOINT.MENU)
+    .then(res => {
+      console.log(res);
+      return res.data;
+    })
+    .catch(err => console.error(err));
+};
